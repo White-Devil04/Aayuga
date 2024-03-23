0a70b10297f473a0cd87c77cce7189fe8bd9e540
Dhvanik Patel <pateldhvanik789@gmail.com>
2024-03-23 23:22:52 +0530

    Managed structure.

---
+++ aayuga-front-end/src/Components/Contact.jsx
@@ -0,0 +1,22 @@
+import logo from '../utils/logo.png';
+function Contact() {
+    return (
+        <div>
+            <div id="contact" className="flex h-72 w-full items-center z-20 bg-blue-300">
+                <div className='mx-8 flex items-center'>
+                    <img src={logo} height='200px' width='200px' className='mx-28' />
+                    <ul>
+                        <li className='m-2 text-lg'><span className='font-semibold'>Email :</span> xyz@aayuga.com</li>
+                        <li className='m-2 text-lg'><span className='font-semibold'>Phone :</span> 1234567890</li>
+                        <li className='m-2 text-lg'><span className='font-semibold'>Address :</span> 123, Aayuga Street, Aayuga City</li>
+                        <li className='m-2 text-lg'><span className='font-semibold'>Pincode :</span> 123456</li>
+                    </ul>
+                </div>
+
+            </div>
+                <footer className='w-full text-center bg-blue-300 text-sm'> @all Rights are Reserved by Aayuga.</footer>
+        </div>
+    );
+}
+
+export default Contact;
--- aayuga-front-end/src/Components/Navbar.jsx
+++ aayuga-front-end/src/Components/Navbar.jsx
@@ -4,29 +4,32 @@ import logo from '../utils/logo.png';
 const Navbar = () => {
 
    const homeScorll = () => {
-      document.getElementById('home').scrollIntoView({behavior:'smooth'});
+      document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
    }
    const aboutScorll = () => {
-      document.getElementById('about').scrollIntoView({behavior:'smooth'});
+      document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
    }
    const servicesScorll = () => {
-      document.getElementById('services').scrollIntoView({behavior:'smooth'});
+      document.getElementById('services').scrollIntoView({ behavior: 'smooth' });
+   }
+   const contactScorll = () => {
+      document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
    }
 
    return (
       <div id="navbar" className='fixed top-0 w-full flex content-between justify-between items-center bg-blue-300 z-50'>
          <div className="flex items-center m-2">
-         <img src={logo} alt="Aayuga" height='50px' width='50px' className='ml-12 mr-3'/>
-         <h1 className='mx-2 text-3xl font-black'>Aayuga</h1>
+            <img src={logo} alt="Aayuga" height='50px' width='50px' className='ml-12 mr-3' />
+            <h1 className='mx-2 text-3xl font-black'>Aayuga</h1>
          </div>
          <div className='flex items-center m-2'>
             <ul className='flex items-center mr-12'>
                <button className='mx-5 text-xl font-semibold' onClick={homeScorll}>Home</button>
                <button className='mx-5 text-xl font-semibold' onClick={aboutScorll}>About</button>
                <button className='mx-5 text-xl font-semibold' onClick={servicesScorll}>Services</button>
-               <button className='mx-5 text-xl font-semibold'>Contact</button>
+               <button className='mx-5 text-xl font-semibold' onClick={contactScorll}>Contact</button>
             </ul>
-            </div>
+         </div>
       </div>
    )
 }
